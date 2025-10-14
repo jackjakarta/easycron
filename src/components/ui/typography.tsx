6610c5fb4ece749cdec27d3c51ee5782dc5ae565
jackjakarta <al.termure@gmail.com>
2025-10-14 03:12:02 +0200

    feat: add policies pages

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -116,3 +116,13 @@ export function TypographyInlineCode({
     </code>
   );
 }
+
+export function TypographyList({
+  children,
+  className,
+}: {
+  children?: React.ReactNode;
+  className?: string;
+}) {
+  return <ul className={cn('my-6 ml-6 list-disc [&>li]:mt-2', className)}>{children}</ul>;
+}
