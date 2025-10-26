8d79c6139339162d0e84b75f872b8d9390f41ae5
Alex Termure <124639246+jackjakarta@users.noreply.github.com>
2025-10-26 18:29:11 +0100

    fix: subscription check api (#29)

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -1,9 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest) {
-  const headers = req.headers;
-  const xApiKey = headers.get('x-api-key');
-  console.debug('Health check received. X-API-Key:', xApiKey);
-
+export async function GET() {
   return NextResponse.json('Ok', { status: 200, statusText: 'Ok' });
 }
--- src/stripe/subscription.ts
+++ src/stripe/subscription.ts
@@ -1,4 +1,5 @@
 import { auth } from '@/auth';
+import { dbGetUserSubscriptions } from '@/db/functions/subscription';
 import { headers } from 'next/headers';
 
 import { FREE_SUBSCRIPTION, PRO_SUBSCRIPTION } from './const';
@@ -15,6 +16,28 @@ export async function getUserActiveSubscription({
     },
     headers: await headers(),
   });
+
+  const activeSubscription = subscriptions.find(
+    (sub) => sub.status === 'active' || sub.status === 'trialing',
+  );
+
+  if (activeSubscription === undefined) {
+    return FREE_SUBSCRIPTION;
+  }
+
+  if (activeSubscription.plan === 'pro') {
+    return PRO_SUBSCRIPTION;
+  }
+
+  return FREE_SUBSCRIPTION;
+}
+
+export async function getUserActiveSubscriptionApi({
+  userId,
+}: {
+  userId: string;
+}): Promise<SubscriptionFeaturesAndLimits> {
+  const subscriptions = await dbGetUserSubscriptions({ userId });
 
   const activeSubscription = subscriptions.find(
     (sub) => sub.status === 'active' || sub.status === 'trialing',
