8b1a2ead67689c670c20b14160987c90845ba8a8
jackjakarta <al.termure@gmail.com>
2026-01-09 14:01:58 +0100

    cicd: update prod compose

---
+++ cicd/update-node-labels.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Manager node (Traefik already constrained by node.role == manager, label optional)
+# docker node update --label-add easycron.role=manager easycron-swarm-manager
+
+docker node update --label-add easycron.role=redis easycron-swarm-redis
+docker node update --label-add easycron.role=app easycron-swarm-worker-1
+docker node update --label-add easycron.role=app easycron-swarm-worker-2
+# docker node update --label-add easycron.role=app easycron-swarm-worker-3
